1117d415ac65725295916baa618fd93b655ae37a
Tomas <tomas.stui@gmail.com>
2024-05-11 14:50:03 +0100

    completed PrefixSumI

--- DSA/app/src/main/java/DSA/PrefixSumI.java
+++ DSA/app/src/main/java/DSA/PrefixSumI.java
@@ -13,31 +13,51 @@ public class PrefixSumI {
     /**
      * @param args the command line arguments
      */
-    private static int PrefixSum1(int[] nums, int[][] queries, int limit){
-        boolean[][] ans = {};
-        int[] cumulativeSum = new int[nums.length];
-        cumulativeSum[0] = nums[0];
-        System.out.println(nums[0]);
+    private static boolean[] PrefixSum1(int[] nums, int[][] queries, int limit){
+        boolean[] ans = new boolean[queries.length];
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        int x, y;
+        int outcome;
         
         for (int i = 1; i < nums.length; i++){
-            System.out.println("Nums: " + nums[i]);
-            cumulativeSum[i] = nums[i] + cumulativeSum[i - 1];
-            System.out.println(cumulativeSum[i]);
+            prefixSum[i] = nums[i] + prefixSum[i - 1];
+            System.out.println("At i: " + i + ", " + prefixSum[i]);
         }
+        
         
         for (int j = 0; j < queries.length; j++){
-            int a = 0;
-            int b = 0;
+            x = queries[j][0];
+            y = queries[j][1];
+            
+            System.out.println("x: " + x);
+            System.out.println("y: " + y);
+            if (x > 0){
+                outcome = prefixSum[y] - prefixSum[x - 1];
+                System.out.println(""+prefixSum[y]+ " "+  prefixSum[x - 1]);
+            } else {
+                outcome = prefixSum[y];
+            }
+            
+            if (outcome <= limit){
+                ans[j] = true;
+            } else {
+                ans[j] = false;
+            }
+            System.out.println("final prefixSum " + outcome);
         }
         
-        return 0;
+        
+        return ans;
     }
     
     public static void main(String args[]) {
         // TODO code application logic here
         int[] arr = {1, 6, 3, 2, 7, 2};
         int[][] queries = {{0, 3}, {2, 5}, {2, 4}};
-        int outcome = PrefixSum1(arr, queries, 3);
-        System.out.println("Outcome: " + outcome);
+        boolean[] outcome = PrefixSum1(arr, queries, 13);
+        for (int p = 0; p < outcome.length; p++){
+            System.out.println("Outcome p: " + outcome[p]);
+        }
     }
 }
